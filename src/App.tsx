d5feee829748243b8ffeed8c2cb1b2eba6a9352d
Sindhuja Govindarjan <srin2258@gmail.com>
2025-01-17 03:53:22 -0500

    Update App with TaxCalculator

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { Suspense } from "react";
-import Layout from "./components/templates/Layout/Layout";
+import Layout from "./components/layout/Layout";
+import TaxCalculator from "./components/taxCalculator/TaxCalculator";
 import "./styles/variables.css";
 
 const App: React.FC = () => {
   return (
     <Layout>
-      <Suspense fallback={<div>Loading...</div>}>TaxCalculator</Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
+        <TaxCalculator />
+      </Suspense>
     </Layout>
   );
 };
